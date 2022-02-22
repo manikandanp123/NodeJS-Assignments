222607a3cf298bc5d2ee2ee1563de9308faab3fa
manikandanp123 <pmanimanu88@gmail.com>
2022-02-22 18:34:20 +0530

    Done 1st assignment

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,25 @@
+const { stdin, stdout } = require("process");
+
+
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    return process.argv[process.argv.length-1];
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name;
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline=require("readline");
+
+    const r1=readline.createInterface({
+        input:stdin,
+        output:stdout
+    });
+    r1.close();
 }
 
 module.exports = {
