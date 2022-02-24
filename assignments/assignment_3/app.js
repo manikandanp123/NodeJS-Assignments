cc5c88fff7a16503fd5e5f6d3a09f84a214de593
manikandanp123 <pmanimanu88@gmail.com>
2022-02-24 16:35:37 +0530

    done 3rd assign

---
+++ assignments/assignment_3/app.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser");
+const faker=require("faker");
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set("views","./views");
+app.set("view engine","ejs");
+
+var i=0;
+var users=[];
+for(var i=0;i<6;i++){
+    users.push({
+        number:i,
+        name:faker.name.findName(),
+        age:26+i,
+        city:faker.address.city(),
+        email:faker.internet.email()
+    })
+}
+app.get("/",(req,res)=>{
+    res.render("homePage.ejs",{users});
+})
+
+app.get("/form",(req,res)=>{
+    res.render("form.ejs")
+})
+
+app.post("/add/user",(req,res)=>{
+    console.log(req.body);
+    users.push({
+        number:i,
+        name:req.body.name,
+        age:req.body.age,
+        city:req.body.city,
+        email:req.body.email
+    })
+    i++;
+    res.redirect('/');
+})
+
+app.listen(4000,()=>console.log("server at  4000"));
